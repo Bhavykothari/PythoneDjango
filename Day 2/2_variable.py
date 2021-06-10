75f4a1eb9a9e94fdcb0828b601f7354a9ca5d07e
Bhavykothari <84842331+Bhavykothari@users.noreply.github.com>
2021-06-10 12:41:58 +0530

    Update 2_variable.py

--- Day 2/2_variable.py
+++ Day 2/2_variable.py
@@ -1,1 +1,23 @@
+name = "Bhavy Kothari"
 
+print("My name is "+ name)
+
+
+
+a,b,c = 10,20.3,"Bhavy Kothari"
+
+print(a)
+
+print(b)
+
+print(c)
+
+
+
+a = b = c = 20
+
+print(a)
+
+print(b)
+
+print(c)
