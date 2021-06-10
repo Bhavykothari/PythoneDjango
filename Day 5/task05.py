732866de3a43244270dac3897b6e4f01134dd863
Bhavykothari <84842331+Bhavykothari@users.noreply.github.com>
2021-06-10 16:07:56 +0530

    Update task05.py

--- Day 5/task05.py
+++ Day 5/task05.py
@@ -1,1 +1,12 @@
+class Employee:
+    def info(self,name,designation):
+        print("Employee's name: ",name)
+        print("Employee's designation: ",designation)
 
+class Sal(Employee):
+    def salary(self,salary):
+        print("Salary is: ",salary)
+
+s = Sal()
+s.info("Bhavy Kothari","Python Developer")
+s.salary(50000)
